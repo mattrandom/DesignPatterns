59954e8e46b733c90cccb50b15b46eb6288024ad
mattrandom <44298255+mattrandom@users.noreply.github.com>
2020-09-02 22:58:38 +0200

    Singleton Pattern - introduction

---
+++ src/main/java/singleton/lesson/Main.java
@@ -0,0 +1,7 @@
+package singleton.lesson;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/singleton/lesson/subpackage/GameEngine.java
@@ -0,0 +1,14 @@
+package singleton.lesson.subpackage;
+
+public class GameEngine {
+    private int hp = 100;
+    private String name;
+
+    public void run() {
+        while (true) {
+            // waiting for the player's reaction with the environment
+            // changing the state of the game
+            // rendering a graphics
+        }
+    }
+}
