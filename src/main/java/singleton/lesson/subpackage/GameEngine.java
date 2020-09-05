4f2c9ac9076611468d8a4bbde4385e94575ff0fa
mattrandom <44298255+mattrandom@users.noreply.github.com>
2020-09-05 10:42:47 +0200

    Singleton Pattern - three ways for  multi-threaded security

--- src/main/java/singleton/lesson/subpackage/GameEngine.java
+++ src/main/java/singleton/lesson/subpackage/GameEngine.java
@@ -5,8 +5,17 @@ public class GameEngine {
     private String name;
 
     private static GameEngine instance;
+    private static GameEngine instance3 = new GameEngine();
 
     private GameEngine() {
+    }
+
+    public void run() {
+        while (true) {
+            // waiting for the player's reaction with the environment
+            // changing the state of the game
+            // rendering a graphics
+        }
     }
 
     public static GameEngine getInstance() {
@@ -16,12 +25,29 @@ public class GameEngine {
         return instance;
     }
 
-    public void run() {
-        while (true) {
-            // waiting for the player's reaction with the environment
-            // changing the state of the game
-            // rendering a graphics
+    // I. Multi-threaded security - synchronized getInstance method:
+    public synchronized static GameEngine getInstance1() {
+        if (instance == null) {
+            instance = new GameEngine();
         }
+        return instance;
+    }
+
+    // II. Multi-threaded security - double check with internal synchronized block:
+    public synchronized static GameEngine getInstance2() {
+        if (instance == null) {
+            synchronized (GameEngine.class) {
+                if (instance == null) {
+                    instance = new GameEngine();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // III. Multi-threaded security - static initialized field
+    public static GameEngine getInstance3() {
+        return instance3;
     }
 }
 
