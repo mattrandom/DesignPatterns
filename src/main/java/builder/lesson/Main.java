c1c32ff4f0bac733fefe3ee9dc4677eb309d6a0d
mattrandom <44298255+mattrandom@users.noreply.github.com>
2020-09-05 16:30:00 +0200

    Builder Pattern - introduction

---
+++ src/main/java/builder/lesson/Main.java
@@ -0,0 +1,9 @@
+package builder.lesson;
+
+import builder.lesson.house.House;
+
+public class Main {
+    public static void main(String[] args) {
+        House house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
+    }
+}
