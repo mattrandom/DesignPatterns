831b66eacd24d64aa2ccd96f3d6b50f86b3bc4d4
mattrandom <44298255+mattrandom@users.noreply.github.com>
2020-09-28 18:44:42 +0200

    Facade Pattern - implementation

--- src/main/java/facade/lesson/Main.java
+++ src/main/java/facade/lesson/Main.java
@@ -1,20 +1,10 @@
 package facade.lesson;
 
-import facade.lesson.deliverybox.DeliveryBox;
-import facade.lesson.deliverybox.DeliveryBoxSystem;
+import facade.lesson.deliverybox.DeliveryBoxFacade;
 
 public class Main {
     public static void main(String[] args) {
-        pickupPackage();
-    }
-
-    private static void pickupPackage() {
-        DeliveryBox deliveryBox = new DeliveryBox();
-        DeliveryBoxSystem deliveryBoxSystem = new DeliveryBoxSystem();
-
-        deliveryBox.getUserData();
-        if (deliveryBoxSystem.isUserDataValidated() && deliveryBoxSystem.isPaymentSecured()) {
-            deliveryBox.openBox();
-        }
+        DeliveryBoxFacade facade = new DeliveryBoxFacade();
+        facade.pickupPackage();
     }
 }
---
+++ src/main/java/facade/lesson/deliverybox/DeliveryBoxFacade.java
@@ -0,0 +1,18 @@
+package facade.lesson.deliverybox;
+
+public class DeliveryBoxFacade {
+    private DeliveryBox deliveryBox;
+    private DeliveryBoxSystem deliveryBoxSystem;
+
+    public DeliveryBoxFacade() {
+        this.deliveryBox = new DeliveryBox();
+        this.deliveryBoxSystem = new DeliveryBoxSystem();
+    }
+
+    public void pickupPackage() {
+        deliveryBox.getUserData();
+        if (deliveryBoxSystem.isUserDataValidated() && deliveryBoxSystem.isPaymentSecured()) {
+            deliveryBox.openBox();
+        }
+    }
+}
